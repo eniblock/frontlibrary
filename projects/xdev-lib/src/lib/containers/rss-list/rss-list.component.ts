cc10b3a89cb50abfbb8e0b74ac9222cee7f31950
amaury calleja <acalleja.ext@theblockchain-group.com>
2020-06-10 00:00:10 +0200

    fix tests

--- projects/xdev-lib/src/lib/containers/rss-list/rss-list.component.ts
+++ projects/xdev-lib/src/lib/containers/rss-list/rss-list.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {NewsRss} from '../../../../../models/interfaces/rss';
+import {NewsRss} from '../../../models/interfaces/rss';
 
 @Component({
   selector: 'app-rss-list',
