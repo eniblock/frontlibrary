aee324ac3c9de733db0c910b4332e9868f2516ce
corentin <corentin.lassalle@outlook.fr>
2021-01-13 12:37:37 +0100

    Fixed rss img

--- projects/xdev-lib/src/lib/components/rss-item/rss-item.component.ts
+++ projects/xdev-lib/src/lib/components/rss-item/rss-item.component.ts
@@ -2,30 +2,33 @@ import {Component, Input, OnInit} from '@angular/core';
 import {IRssItem} from '../../../models/interfaces/rss';
 
 @Component({
-  selector: 'app-rss-item',
-  templateUrl: './rss-item.component.html',
-  styleUrls: ['./rss-item.component.scss']
+    selector: 'app-rss-item',
+    templateUrl: './rss-item.component.html',
+    styleUrls: ['./rss-item.component.scss']
 })
 export class RssItemComponent implements OnInit {
 
-  @Input() rssItem: IRssItem;
+    @Input() rssItem: IRssItem;
 
-  extractContent = (s: string) => {
-    let content = s.replace(/<img[^>]*>/g, '');
-    content = content.replace( /(<([^>]+)>)/ig, '');
-    const span = document.createElement('span');
-    span.innerHTML = content;
-    return span.textContent || span.innerText;
-  }
+    extractContent = (s: string) => {
+        let content = s.replace(/<img[^>]*>/g, '');
+        content = content.replace(/(<([^>]+)>)/ig, '');
+        const span = document.createElement('span');
+        span.innerHTML = content;
+        return span.textContent || span.innerText;
+    }
 
-  extractImg = (s: string) => {
-    const img = s.match(/\<img.+src\=(?:\"|\')(.+?)(?:\"|\')(?:.+?)\>/);
-    return img && img.length > 1 ? img[1] : null;
-  }
+    extractImg = (content: any) => {
+        if (content && content[0]) {
+            return content[0].$.url;
+        }
+        return null;
+    }
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
